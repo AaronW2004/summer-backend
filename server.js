431e4c99ff4c33a6da5b8e378d6fd170fd342e0e
AaronW2004 <anwilliams2004@gmail.com>
2025-06-18 17:24:06 -0400

    deploy please

--- server.js
+++ server.js
@@ -1,15 +1,88 @@
 const express = require("express");
 const app = express();
+app.use(express.static("public"));
+app.use(express.json());
 const cors = require('cors');
+app.use(cors({ origin: '*' }));
+
 const multer = require("multer");
 const path = require("path");
 const Joi = require('joi');
 
-app.use(cors({ origin: '*' }));
-app.use(express.static("public"));
-app.use(express.json());
-
-let graduates = [ /* your existing data */ ];
+let graduates = [
+  {
+    "_id": 1,
+    "name": "Imani Johnson",
+    "classification": "Senior",
+    "major": "Biology",
+    "awards": "Dean's List, Spelman Research Scholar",
+    "latinHonors": "Summa Cum Laude",
+    "img_name": "/images/graduate1.jpg"
+  },
+  {
+    "_id": 2,
+    "name": "Chloe Smith",
+    "classification": "Senior",
+    "major": "Computer Science",
+    "awards": "Google Scholar, Hackathon Winner",
+    "latinHonors": "Magna Cum Laude",
+    "img_name": "/images/graduate2.jpg"
+  },
+  {
+    "_id": 3,
+    "name": "Aaliyah Davis",
+    "classification": "Senior",
+    "major": "Economics",
+    "awards": "National Merit Scholar",
+    "latinHonors": "Cum Laude",
+    "img_name": "/images/graduate3.jpg"
+  },
+  {
+    "_id": 4,
+    "name": "Jada Brown",
+    "classification": "Senior",
+    "major": "Political Science",
+    "awards": "Spelman Leadership Award",
+    "latinHonors": "Cum Laude",
+    "img_name": "/images/graduate4.jpg"
+  },
+  {
+    "_id": 5,
+    "name": "Kayla Moore",
+    "classification": "Senior",
+    "major": "Mathematics",
+    "awards": "STEM Scholar",
+    "latinHonors": "Magna Cum Laude",
+    "img_name": "/images/graduate5.jpg"
+  },
+  {
+    "_id": 6,
+    "name": "Zuri Taylor",
+    "classification": "Senior",
+    "major": "Art History",
+    "awards": "Creative Excellence Grant",
+    "latinHonors": "Cum Laude",
+    "img_name": "/images/graduate6.jpg"
+  },
+  {
+    "_id": 7,
+    "name": "Morgan Lee",
+    "classification": "Senior",
+    "major": "Psychology",
+    "awards": "Community Impact Award",
+    "latinHonors": "Summa Cum Laude",
+    "img_name": "/images/graduate7.jpg"
+  },
+  {
+    "_id": 8,
+    "name": "Amara Green",
+    "classification": "Senior",
+    "major": "English",
+    "awards": "Writing Award, Dean's List",
+    "latinHonors": "Magna Cum Laude",
+    "img_name": "/images/graduate8.jpg"
+  }
+];
 
 const validateGraduate = (data) => {
   const schema = Joi.object({
@@ -33,10 +106,16 @@ const storage = multer.diskStorage({
     cb(null, uniqueName);
   }
 });
+
 const upload = multer({ storage });
 
-app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
-app.get("/api/graduates", (req, res) => res.json(graduates));
+app.get("/", (req, res)=> {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/api/graduates", (req, res) => {
+    res.json(graduates);
+});
 
 app.post("/api/graduates", upload.single("image"), (req, res) => {
   const { error, value } = validateGraduate(req.body);
